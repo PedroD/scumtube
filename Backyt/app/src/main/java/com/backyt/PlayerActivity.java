e3dd2adc8be53587a9d4ad54477093034d4c3e93
voxivoid <voxivoid@hotmail.com>
2015-07-31 22:07:37 +0100

    Refactoring

--- Backyt/app/src/main/java/com/backyt/PlayerActivity.java
+++ Backyt/app/src/main/java/com/backyt/PlayerActivity.java
@@ -1,26 +1,10 @@
 package com.backyt;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.concurrent.ExecutionException;
 
 
 public class PlayerActivity extends ActionBarActivity {
@@ -30,75 +14,14 @@ public class PlayerActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         if (getIntent().getExtras() != null){
             Bundle extras = getIntent().getExtras();
-            String ytLink = extras.getString(Intent.EXTRA_TEXT);
-            String streamUrl = null;
-            try {
-                streamUrl = new RequestMp3().execute(parseVideoId(ytLink)).get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-            Log.i("Youtube", ytLink);
-            Log.i("VAGINA", PlayerService.class.getName());
+            String ytUrl = extras.getString(Intent.EXTRA_TEXT);
             Intent playerService = new  Intent(this, PlayerService.class);
             playerService.setAction(PlayerService.ACTION_PLAY);
-            playerService.putExtra("streamUrl", streamUrl);
+            playerService.putExtra("ytUrl", ytUrl);
             startService(playerService);
-
         }
         finish();
     }
-
-    private String parseVideoId(String url){
-        String [] splittedUrl = url.split("/");
-        return splittedUrl[3]; //splittedUrl[3] is the id of the video
-    }
-
-    class RequestMp3 extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... videoId) {
-            String requestUrl = "http://wavedomotics.com:9194/video_id/" + videoId[0];
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpGet httpGet = new HttpGet(requestUrl);
-            try{
-                JSONObject jsonObject;
-                while(true){
-                    HttpResponse httpResponse = httpClient.execute(httpGet);
-                    HttpEntity httpEntity = httpResponse.getEntity();
-                    if(httpEntity != null) {
-                        InputStream inputStream = httpEntity.getContent();
-
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                        StringBuilder stringBuilder = new StringBuilder();
-
-                        String line = bufferedReader.readLine();
-                        while (line != null) {
-                            stringBuilder.append(line + " \n");
-                            line = bufferedReader.readLine();
-                        }
-                        bufferedReader.close();
-
-                        jsonObject = new JSONObject(stringBuilder.toString());
-                        if(jsonObject.has("ready")){
-                            break;
-                        }
-                        Log.i("Pedido", jsonObject.getString("scheduled"));
-                        Thread.sleep(5000);
-                    }
-                }
-                Log.i("Pedido", jsonObject.getString("url"));
-                return jsonObject.getString("url");
-
-        } catch (IOException e){
-        } catch (JSONException e){
-        } catch (InterruptedException e) {
-            }
-            return "";
-        }
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Backyt/app/src/main/java/com/backyt/PlayerService.java
+++ Backyt/app/src/main/java/com/backyt/PlayerService.java
@@ -8,14 +8,28 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
 
 
 public class PlayerService extends Service {
@@ -74,14 +88,19 @@ public class PlayerService extends Service {
                 updateNotification();
             }
         });
-
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(intent.getExtras() != null){
-           streamUrl = intent.getExtras().getString("streamUrl");
+            String ytUrl = intent.getExtras().getString("ytUrl");
+            try {
+                streamUrl = new RequestMp3().execute(parseVideoId(ytUrl)).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
         if (intent != null && intent.getAction() != null) {
             if (intent.getAction().equals(ACTION_PLAYPAUSE)) {
@@ -95,6 +114,55 @@ public class PlayerService extends Service {
             }
         }
         return START_STICKY;
+    }
+
+    private String parseVideoId(String url){
+        String [] splittedUrl = url.split("/");
+        return splittedUrl[3]; //splittedUrl[3] is the id of the video
+    }
+
+    class RequestMp3 extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... videoId) {
+            String requestUrl = "http://wavedomotics.com:9194/video_id/" + videoId[0];
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(requestUrl);
+            try{
+                JSONObject jsonObject;
+                while(true){
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    HttpEntity httpEntity = httpResponse.getEntity();
+                    if(httpEntity != null) {
+                        InputStream inputStream = httpEntity.getContent();
+
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        String line = bufferedReader.readLine();
+                        while (line != null) {
+                            stringBuilder.append(line + " \n");
+                            line = bufferedReader.readLine();
+                        }
+                        bufferedReader.close();
+
+                        jsonObject = new JSONObject(stringBuilder.toString());
+                        if(jsonObject.has("ready")){
+                            break;
+                        }
+                        Log.i("Pedido", jsonObject.getString("scheduled"));
+                        Thread.sleep(5000);
+                    }
+                }
+                Log.i("Pedido", jsonObject.getString("url"));
+                return jsonObject.getString("url");
+
+            } catch (IOException e){
+            } catch (JSONException e){
+            } catch (InterruptedException e) {
+            }
+            return "";
+        }
     }
 
     public void start() {
