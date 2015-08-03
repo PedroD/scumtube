fda88b1cea6ecda330b97588f5043165bf0b4075
Pedro Domingues <pedro.domingues@ist.utl.pt>
2015-08-03 14:42:49 +0100

    Updated acivity

--- Backyt/app/src/main/java/com/backyt/PlayerActivity.java
+++ Backyt/app/src/main/java/com/backyt/PlayerActivity.java
@@ -1,46 +1,29 @@
 package com.backyt;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class PlayerActivity extends ActionBarActivity {
+public class PlayerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getIntent().getExtras() != null){
-            Bundle extras = getIntent().getExtras();
-            String ytUrl = extras.getString(Intent.EXTRA_TEXT);
-            Intent playerService = new  Intent(this, PlayerService.class);
-            playerService.setAction(PlayerService.ACTION_PLAY);
-            playerService.putExtra("ytUrl", ytUrl);
-            startService(playerService);
+        if (getIntent().getExtras() != null) {
+            final Bundle extras = getIntent().getExtras();
+            final String ytUrl = extras.getString(Intent.EXTRA_TEXT);
+            if (ytUrl.contains("youtube.com")) {
+                final Intent playerService = new Intent(this, PlayerService.class);
+                playerService.setAction(PlayerService.ACTION_PLAY);
+                playerService.putExtra("ytUrl", ytUrl);
+                startService(playerService);
+            } else {
+                Toast.makeText(getApplicationContext(), "Error: Wrong URL (" + ytUrl + ")!", Toast.LENGTH_LONG).show();
+            }
         }
+        finish();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_player, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
