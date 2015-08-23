e87158d82600f428050a76f09a9a7a234dab0433
Pedro Domingues <pedro.domingues@ist.utl.pt>
2015-08-23 11:51:22 +0100

    Version validation working.

--- ScumTube/app/src/main/java/com/scumtube/ScumTubeApplication.java
+++ ScumTube/app/src/main/java/com/scumtube/ScumTubeApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.os.Build;
 import android.widget.RemoteViews;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ScumTubeApplication extends Application {
 
     public static final String APP_NAME = "ScumTube";
@@ -40,5 +44,22 @@ public class ScumTubeApplication extends Application {
         }
     }
 
+    public static String md5(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+        }
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            int i = (b & 0xFF);
+            if (i < 0x10) hex.append('0');
+            hex.append(Integer.toHexString(i));
+        }
+        return hex.toString();
+    }
 
 }
