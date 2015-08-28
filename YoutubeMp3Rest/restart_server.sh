5453b919bbff441d29a6da958a04cf1c56461efd
Pedro Domingues <pedro.domingues@ist.utl.pt>
2015-08-28 22:27:02 +0100

    no message

--- YoutubeMp3Rest/restart_server.sh
+++ YoutubeMp3Rest/restart_server.sh
@@ -2,7 +2,7 @@ echo "KILLING OLD SERVER SESSION"
 kill $(cat lastPID.txt)
 sleep 3
 echo "STARTING SERVER..."
-# java -Dfile.encoding=utf-8 -jar youtube.to.mp3-0.0.1-SNAPSHOT-jar-with-dependencies.jar </dev/null>/dev/null 2>&1 &
-java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9199 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=176.111.109.11 -jar youtube.to.mp3-0.0.1-SNAPSHOT-jar-with-dependencies.jar </dev/null>/dev/null 2>&1 &
+java -Dfile.encoding=utf-8 -jar youtube.to.mp3-0.0.1-SNAPSHOT-jar-with-dependencies.jar </dev/null>/dev/null 2>&1 &
+# java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9199 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=176.111.109.11 -jar youtube.to.mp3-0.0.1-SNAPSHOT-jar-with-dependencies.jar </dev/null>/dev/null 2>&1 &
 echo $! > lastPID.txt
 echo "SERVER STARTED! PID:" $(cat lastPID.txt)
